d4cf9e6f0427dc57b292a14b73da1974dd49b597
gmescudero <german.moreno.escudero@alumnos.upm.es>
2019-10-03 05:50:37 -0500

    aded nemo to gitignore and adjusted shell script to launch on gazebo with controllers

--- run_gazebo.sh
+++ run_gazebo.sh
@@ -3,4 +3,5 @@
 sudo killall rosmaster
 sudo killall gzserver
 sudo killall gzclient
-roslaunch trybot_gazebo trybot_world.launch
+#roslaunch trybot_gazebo trybot_world.launch
+roslaunch trybot_gazebo trybot_gzcontrol.launch
