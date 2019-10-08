59842686b7852bc1a0b05f84c8d028971ddd579d
gmescudero <german.moreno@gtd.eu>
2019-10-08 03:11:12 -0500

    new 6DOF_Cartesian robot

--- run_rviz.sh
+++ run_rviz.sh
@@ -1,1 +1,35 @@
-roslaunch trybot_description trybot_rviz.launch
+#!/bin/sh
+
+helpFunction()
+{
+    echo "This script automatically launches rviz visualizer for a given robot"
+    echo "Usage: $0 [<robot>]"
+    echo "Available robots:"
+    echo "\t-t trybot: 3DOF cartesian rviz simulation"
+    echo "\t-n nemo: launches the braccio rviz simulation"
+    echo "\t-c 6DOFCartesian: 6DOF cartesian rviz simulation (WIP)"
+    echo "\t-h help: print this info"
+    exit 1
+}
+
+
+if [ $# = 0 ]; then helpFunction; fi
+
+while getopts "tnch" opt; do
+    case $opt in
+        t) # Trybot
+            echo "Launching trybot simulation"
+            roslaunch trybot_description trybot_rviz.launch
+            ;;
+        n) # Nemo
+            echo "Launching nemo simulation"
+            roslaunch nemo nemo_rviz.launch
+            ;;
+        c) # Cartesian
+            echo "Launching cartesian simulation (WIP)"
+            roslaunch 6DOFCartesian 6DOFCartesian_rviz.launch
+            ;;
+        h) helpFunction ;; #Print help
+        ?) helpFunction ;; #Print help
+    esac
+done
